16cf74bf4661c01872ccab2777c79fa8550a8f40
Климентьев Андрей <andrey.klimentev@gazprombank.ru>
2025-12-21 14:54:59 +0300

    feat: add app users and migrations

---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите аватар",
+    )
+    city = models.CharField(
+        max_length=255, verbose_name="Город", help_text="Введите город проживания"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
